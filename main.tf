adea36f1f08dbb30269386d977c8d6a1e3b13014
PhiChat <estudotechnologies@gmail.com>
2026-02-03 11:44:56 +0530

    learn1 : demo : connecting azure

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "nsrgexample" {
+  name     = "rg-terraform-github-actions"
+  location = "eastus"
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "subscription_id" {}
+variable "client_id" {}
+variable "client_secret" {
+  sensitive = true
+}
+variable "tenant_id" {}
