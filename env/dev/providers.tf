b94d7df6268d172427e7fdfe464796bbc1fa1814
PhiChat <estudotechnologies@gmail.com>
2026-02-05 11:48:28 +0530

    fixing path error : change directory before plan execution : try1

--- env/dev/providers.tf
+++ env/dev/providers.tf
@@ -14,8 +14,9 @@ provider "azurerm" {
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
+}
+
 #   subscription_id = "ea4756b6-8b19-4ee8-9491-0ee7f49c536d"
 #   client_id       = "6ea2054a-43be-4324-bcdc-30c457fec84f"
 #   client_secret   = var.client_secret
 #   tenant_id       = "213e6d57-b40c-4684-8287-8453b6d2f698"
-}
